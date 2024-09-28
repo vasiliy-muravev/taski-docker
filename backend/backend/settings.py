527a1fea7ef1951e6fafed14c72b82214b22a3d6
vasiliy-muravev <vasiliymuravev@yandex.ru>
2024-09-29 01:57:54 +0300

    Добавить workflows

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -9,7 +9,7 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['84.252.141.216', '127.0.0.1', 'localhost', 'vasiliymuravev.serverhttp.com']
+ALLOWED_HOSTS = ['84.252.141.216', '127.0.0.1', 'localhost', 'vasiliymuravev.serveftp.com']
 
 
 # Application definition
